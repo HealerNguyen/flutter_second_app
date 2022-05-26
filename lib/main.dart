c962ae60a5851a288acadc687e310c804c1ac186
Viruss98 <nguyenductuan98tb@gmail.com>
2022-05-26 23:23:45 +0700

    add list transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:second_app/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,11 +17,63 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(1, 'Pay for google api service', 11.12, DateTime.now()),
+    Transaction(2, 'Pay for aws api service', 11.19, DateTime.now()),
+    Transaction(3, 'Pay for cloud api service', 11.99, DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
-      body: Center(child: Text('body app')),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.green,
+                child: Text(
+                  'Chart',
+                  style: TextStyle(color: Colors.white),
+                ),
+                elevation: 1,
+              ),
+            ),
+            Column(
+                children: transactions.map((transaction) {
+              return Card(
+                child: Row(children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 2),
+                    ),
+                    child: Text(
+                      '\$${transaction.amount}',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        transaction.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 16),
+                      ),
+                      Text(transaction.date.toString(),
+                          style: TextStyle(color: Colors.black54))
+                    ],
+                  )
+                ]),
+              );
+            }).toList())
+          ]),
     );
   }
 }
---
+++ lib/transaction.dart
@@ -0,0 +1,8 @@
+class Transaction {
+  double id;
+  String title;
+  double amount;
+  DateTime date;
+
+  Transaction(this.id, this.title, this.amount, this.date);
+}
